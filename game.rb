4386be4961484407378d55956e8c3d6e4b7b4dfa
Enrique Carral Trevino <ecarralt@gmail.com>
2016-06-07 15:13:59 -0500

    done with first version of the game

--- game.rb
+++ game.rb
@@ -4,6 +4,40 @@ class Game
   def play
     puts "Ready to play? Choose (r)ock, (p)aper or (s)cissors. Just write the first letter of each"
     @player_one_choice = gets.chomp
+    puts "your choice is #{player_one_choice}"
+
+    game_choices = ["r", "p", "s"]
+
+    @computer = game_choices.sample
+
+    puts "The computer's choice is #{computer}"
+
+    case player_one_choice
+    when "r"
+      if computer == "r"
+        puts "IT'S A TIE"
+      elsif computer == "p"
+        puts "YOU LOSE"
+      elsif computer == "s"
+        puts "YOU WIN"
+      end
+    when "p"
+      if computer == "r"
+        puts "YOU WIN"
+      elsif computer == "p"
+        puts "IT'S A TIE"
+      elsif computer == "s"
+        puts "YOU LOSE"
+      end
+    when "s"
+      if computer == "r"
+        puts "YOU LOSE"
+      elsif computer == "p"
+        puts "YOU WIN"
+      elsif computer == "s"
+        puts "IT'S A TIE"
+      end
+    end
 
   end
 
